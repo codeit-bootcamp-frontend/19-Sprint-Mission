6237c871fa5df0f8207a0f46d43d7ce121ade6ea
jerryko570 <jerry.narae@gmail.com>
2025-08-30 10:56:55 +0900

    스프린트 2/3 파일 추가

---
+++ script.js
@@ -0,0 +1,18 @@
+//요소 찾기
+const nameInput = document.querySelector("#name");
+const nameError = document.querySelector("#nameError");
+const btn = document.querySelector("#btn");
+
+
+//검사 규칙 짜기
+if (nameInput === '') {
+  
+}
+
+function checkName() {
+  const text = nameInfo.value.trim();
+
+  if (text === ''){
+    nameError.textContent
+  }
+}
