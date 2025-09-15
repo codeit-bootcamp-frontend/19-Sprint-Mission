bc54b63cceff65a0e92a262b398189797ecf1e00
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-15 22:11:31 +0900

    ✨ Feat: 자유게시판 페이지 추가

---
+++ src/pages/Boards.jsx
@@ -0,0 +1,9 @@
+const Boards = () => {
+  return (
+    <div>
+      <p>자유게시판</p>
+    </div>
+  );
+};
+
+export default Boards;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
+import MainLayout from '@/layouts/mainLayout/MainLayout';
 import AddItem from '@/pages/AddItem';
+import Boards from '@/pages/Boards';
 import Home from '@/pages/Home';
 import Items from '@/pages/Items';
 import Login from '@/pages/Login';
@@ -9,11 +11,14 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/boards" element={<Boards />} />
+          <Route path="/additem" element={<AddItem />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/items" element={<Items />} />
-        <Route path="/additem" element={<AddItem />} />
       </Routes>
     </BrowserRouter>
   );
