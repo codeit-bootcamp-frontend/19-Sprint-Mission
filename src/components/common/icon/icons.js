3dd015882d4295f41a46ac83f30959d708087822
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-15 22:47:05 +0900

    🚚 Rename: 아이콘 import 관리용 파일 생성

---
+++ src/components/common/icon/icons.js
@@ -0,0 +1,6 @@
+import FacebookIcon from '@/assets/icons/ic-facebook.svg';
+import InstgramIcon from '@/assets/icons/ic-instagram.svg';
+import TwitterIcon from '@/assets/icons/ic-twitter.svg';
+import YoutubeIcon from '@/assets/icons/ic-youtube.svg';
+
+export default { FacebookIcon, InstgramIcon, TwitterIcon, YoutubeIcon };
