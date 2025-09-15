744fdb9d53d03ea49efdcb0026f72c89a21b969c
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-15 20:22:15 +0900

    ⚙️ Chore: vite svgr 플러그인 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr({ include: '**/*.svg' })],
   resolve: {
     alias: {
       '@': '/src',
