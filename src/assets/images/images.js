95178ecb7c25f7eb50470932bab78eeafc4ff9ec
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-16 12:07:43 +0900

    ✨ Feat: 이미지 에셋, import 파일 추가

---
+++ src/assets/images/images.js
@@ -0,0 +1,19 @@
+import authGoogle from '@/assets/images/auth/google.png';
+import authKakao from '@/assets/images/auth/kakao.png';
+import profile from '@/assets/images/gnb/profile.png';
+import homeOverview1 from '@/assets/images/home/img-home-01.png';
+import homeOverview2 from '@/assets/images/home/img-home-02.png';
+import homeOverview3 from '@/assets/images/home/img-home-03.png';
+import homeBottomBanner from '@/assets/images/home/img-home-bottom.png';
+import homeTopBanner from '@/assets/images/home/img-home-top.png';
+
+export default {
+  authGoogle,
+  authKakao,
+  profile,
+  homeOverview1,
+  homeOverview2,
+  homeOverview3,
+  homeBottomBanner,
+  homeTopBanner,
+};
