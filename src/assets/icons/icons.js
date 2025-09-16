1a175679d5b5d10d5d710eaded61aef4cadb2691
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-16 16:14:58 +0900

    ✨ Feat: 비밀번호 가시성 토글 아이콘 추가

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -1,6 +1,15 @@
 import facebookIcon from '@/assets/icons/ic-facebook.svg?url';
 import instgramIcon from '@/assets/icons/ic-instagram.svg?url';
 import twitterIcon from '@/assets/icons/ic-twitter.svg?url';
+import HidePasswordIcon from '@/assets/icons/ic-visibility-off.svg';
+import ShowPasswordIcon from '@/assets/icons/ic-visibility-on.svg';
 import youtubeIcon from '@/assets/icons/ic-youtube.svg?url';
 
-export default { facebookIcon, instgramIcon, twitterIcon, youtubeIcon };
+export default {
+  facebookIcon,
+  instgramIcon,
+  twitterIcon,
+  youtubeIcon,
+  HidePasswordIcon,
+  ShowPasswordIcon,
+};
