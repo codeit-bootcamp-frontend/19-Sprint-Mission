f987c06628da44cb14de7d2c99cc114194bad17c
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-16 14:44:21 +0900

    ✨ Feat: 홈 컴포넌트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
+import Footer from '@/components/common/footer/Footer';
+import Gnb from '@/components/common/gnb/Gnb';
+import BottomBanner from '@/components/Home/banner/BottomBanner';
+import TopBanner from '@/components/Home/banner/TopBanner';
+import Overview from '@/components/Home/overview/Overview';
+import styles from '@/style/page/Home.module.css';
+
 const Home = () => {
   return (
-    <div>
-      <p>홈페이지</p>
-    </div>
+    <>
+      <Gnb login={false} />
+      <main className={styles.container}>
+        <TopBanner />
+        <Overview />
+        <BottomBanner />
+      </main>
+      <Footer />
+    </>
   );
 };
 
