f70363fedb405fcc7a023c35a1764f6f220dbf73
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-17 10:28:08 +0900

    ✨ Feat: 공통 Input 컴포넌트 props 추가

--- src/components/common/input/Input.jsx
+++ src/components/common/input/Input.jsx
@@ -1,13 +1,27 @@
 import styles from './Input.module.css';
 
-const Input = ({ className = '', ...props }) => {
+const Input = ({
+  className = '',
+  id,
+  type = 'text',
+  placeholder,
+  ...props
+}) => {
   const { error, value } = props;
 
   const inputClassName = `${styles.input} ${
     error ? styles['input-error'] : ''
   } ${value && !error ? styles['input-complete'] : ''} ${className}`;
 
-  return <input className={inputClassName} {...props} />;
+  return (
+    <input
+      className={inputClassName}
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      {...props}
+    />
+  );
 };
 
 export default Input;
