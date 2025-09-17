ca5ad83118ee5184742b3daf9856014b59aa681b
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-17 11:08:23 +0900

    ✨ Feat: 화살표 아이콘, 검색 아이콘 추가

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -1,14 +1,18 @@
+import ArrowDownIcon from '@/assets/icons/ic-arrow-down.svg';
 import facebookIcon from '@/assets/icons/ic-facebook.svg?url';
 import HeartIcon from '@/assets/icons/ic-heart.svg';
 import instgramIcon from '@/assets/icons/ic-instagram.svg?url';
+import SearchIcon from '@/assets/icons/ic-search.svg';
 import twitterIcon from '@/assets/icons/ic-twitter.svg?url';
 import HidePasswordIcon from '@/assets/icons/ic-visibility-off.svg';
 import ShowPasswordIcon from '@/assets/icons/ic-visibility-on.svg';
 import youtubeIcon from '@/assets/icons/ic-youtube.svg?url';
 
 export default {
+  ArrowDownIcon,
   facebookIcon,
   instgramIcon,
+  SearchIcon,
   twitterIcon,
   youtubeIcon,
   HidePasswordIcon,
