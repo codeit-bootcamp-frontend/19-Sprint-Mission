4b60dfac1066ec445b73bca089bf8a483eb6ea7c
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-17 16:27:44 +0900

    ✨ Feat: 상품 리스트 api 함수 추가

--- src/apis/product.js
+++ src/apis/product.js
@@ -1,6 +1,15 @@
 import { api } from '@/apis/instance';
 
-export const getBestProducts = async () => {
-  const res = await api.get('/products?page=1&pageSize=4&orderBy=favorite');
+export const getBestProducts = async ({ pageSize }) => {
+  const res = await api.get(
+    `/products?page=1&pageSize=${pageSize}&orderBy=favorite`
+  );
+  return res.data;
+};
+
+export const getProducts = async ({ page, pageSize, orderBy = 'recent' }) => {
+  const res = await api.get(
+    `/products?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`
+  );
   return res.data;
 };
