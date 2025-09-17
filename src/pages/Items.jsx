a609977877a23629f32af3906d9fb8734ef455b9
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-17 12:02:01 +0900

    ✨ Feat: 베스트 상품, 전체상품 top 영역 구현

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router';
 import { getBestProducts } from '@/apis/product';
+import icons from '@/assets/icons/icons';
+import Button from '@/components/common/button/Button';
+import Input from '@/components/common/input/Input';
 import Title from '@/components/common/title/Title';
 import Card from '@/components/items/card/Card';
+import SelectBox from '@/components/items/selectBox/SelectBox';
+import { SORT_OPTIONS } from '@/constants/sortOptions';
 import styles from '@/style/page/Items.module.css';
 
 const Items = () => {
@@ -20,6 +26,10 @@ const Items = () => {
 
     fetchProducts();
   }, []);
+
+  const handleSort = (sortValue) => {
+    console.log(`서버에 보낼 값: ${sortValue}`);
+  };
 
   if (error) {
     return <div>에러 발생 : {error.message}</div>;
@@ -43,8 +53,23 @@ const Items = () => {
           })}
         </div>
       </section>
-      <section>
-        <Title>전체 상품</Title>
+      <section className={styles['product-area']}>
+        <div className={styles['top-area']}>
+          <Title>전체 상품</Title>
+          <div className={styles['search-area']}>
+            <icons.SearchIcon className={styles['search-icon']} />
+            <Input
+              id="search"
+              className={styles['search-input']}
+              placeholder="검색할 상품을 입력해주세요"
+            />
+          </div>
+          <Button as={Link} to="/additem" size="xs">
+            상품 등록하기
+          </Button>
+          <SelectBox options={SORT_OPTIONS} onSelect={handleSort} />
+        </div>
+        <div className={styles.contents} />
       </section>
     </main>
   );
