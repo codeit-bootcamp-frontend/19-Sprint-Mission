2c05d125f70d93ec89d3df38ef1c77a0a80e7290
Dang <wheodnjs2@naver.com>
2025-10-28 16:37:54 +0900

    메인 페이지 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import Header from './components/Header';
+import ProductCard from './components/ProductCard';
+import Title from './components/Title';
+import styled from 'styled-components';
+
 function App() {
-  return <>판다마켓</>;
+  return (
+    <>
+      <Header />
+      <ContentBody>
+        <Title title="베스트 상품" />
+        <ProductCard />
+        <Title title="전체 상품" />
+      </ContentBody>
+    </>
+  );
 }
 
 export default App;
+
+const ContentBody = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 24px auto 0;
+`;
