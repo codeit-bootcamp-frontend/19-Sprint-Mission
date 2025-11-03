c0de89488d8b5726a01a030beb483069f9609b5e
Dang <wheodnjs2@naver.com>
2025-11-04 01:44:54 +0900

    home 페이지 수정

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,30 +1,100 @@
+import { getProducts } from '../utill/api';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import Button from '../components/Button';
 import Dropdown from '../components/Dropdown';
 import Input from '../components/Input';
 import Pagination from '../components/Pagination';
 import ProductList from '../components/ProductList';
 import Title from '../components/Title';
-import styled from 'styled-components';
 
 function Home() {
+  const [best, setBest] = useState([]);
+  const [all, setAll] = useState([]);
+  const [dropState, setDropState] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [bigPageSize, setBigPageSize] = useState(4);
+
+  useEffect(() => {
+    const fetchBest = async () => {
+      const res = await getProducts(1, bigPageSize, 'favorite');
+      setBest(res.list);
+    };
+
+    fetchBest();
+  }, [bigPageSize]);
+
+  useEffect(() => {
+    const fetchAll = async () => {
+      const order = dropState ? 'recent' : 'favorite';
+      const res = await getProducts(currentPage, pageSize, order);
+      setAll(res.list);
+
+      if (res.totalCount) {
+        setTotalPages(Math.ceil(res.totalCount / pageSize));
+      }
+    };
+    fetchAll();
+  }, [dropState, currentPage, pageSize]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      let newPageSize;
+      let newBigPageSize;
+
+      if (width <= 600) {
+        newPageSize = 4;
+        newBigPageSize = 1;
+      } else if (width <= 900) {
+        newPageSize = 6;
+        newBigPageSize = 2;
+      } else {
+        newPageSize = 10;
+        newBigPageSize = 4;
+      }
+
+      if (newPageSize !== pageSize) {
+        setPageSize(newPageSize);
+        setBigPageSize(newBigPageSize);
+        setCurrentPage(1);
+      }
+    };
+
+    handleResize(); // 초기 실행
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [pageSize]);
+
   return (
     <>
       <ContentBody>
         <Title title="베스트 상품" />
-        <ProductList />
+        <ProductList product={best} size="big" />
 
         <AllProduct>
           <div>
             <Title title="전체 상품" />
             <div>
               <Input />
-              <Button buttonName="상품 등록하기" />
-              <Dropdown />
+              <Button buttonName="상품 등록하기" to={'/addItem'} />
+              <Dropdown dropState={dropState} setDropState={setDropState} />
             </div>
           </div>
-          <ProductList />
+          <ProductList product={all} size="small" />
         </AllProduct>
-        <Pagination />
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
       </ContentBody>
     </>
   );
@@ -36,6 +106,11 @@ const ContentBody = styled.div`
   width: 100%;
   max-width: 1200px;
   margin: 24px auto 0;
+
+  // 테블릿
+  @media (max-width: 900px) {
+    padding: 0 24px;
+  }
 `;
 
 const AllProduct = styled.div`
@@ -48,12 +123,51 @@ const AllProduct = styled.div`
     > div {
       display: flex;
 
-      > button {
+      > button,
+      > a {
         margin-left: 12px;
       }
 
       > div {
         margin-left: 12px;
+      }
+    }
+  }
+
+  // 테블릿
+  @media (max-width: 900px) {
+    > div input {
+      width: 242px;
+    }
+  }
+
+  // 모바일
+  @media (max-width: 600px) {
+    > div {
+      display: block;
+      position: relative;
+      > h2 {
+        width: 100%;
+      }
+
+      > div {
+        margin-top: 14px;
+        align-items: center;
+        > button {
+          position: absolute;
+          right: 0;
+          top: -9px;
+          height: 42px;
+        }
+
+        > div {
+          width: 42px;
+        }
+
+        input {
+          width: calc(100% - 53px);
+          height: 42px;
+        }
       }
     }
   }
