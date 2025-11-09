5bdc8282898c8771aabebef9ca1e8840f5d46253
Dang <wheodnjs2@naver.com>
2025-11-10 02:11:08 +0900

    배포 오류 수정 2

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  build: {
+    outDir: 'dist', // 기본값 (Netlify에선 dist가 배포 폴더)
+  },
 });
