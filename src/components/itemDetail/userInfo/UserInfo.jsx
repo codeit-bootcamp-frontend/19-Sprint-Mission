36e3d9766cdda2bc32d2120dab419a709f2d6b86
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-11 00:38:03 +0900

    ✨ Feat: UserInfo size props 추가

--- src/components/itemDetail/userInfo/UserInfo.jsx
+++ src/components/itemDetail/userInfo/UserInfo.jsx
@@ -1,11 +1,16 @@
+import classNames from 'classnames';
 import ProfileImg from '@/components/common/profileImg/ProfileImg';
 import styles from './UserInfo.module.css';
 
-const UserInfo = ({ nickname, date }) => {
+const UserInfo = ({ nickname, date, size = 'm' }) => {
   return (
     <div className={styles.container}>
-      <ProfileImg />
-      <div className={styles['info-area']}>
+      <ProfileImg size={size} />
+      <div
+        className={classNames(styles['info-area'], {
+          [styles['info-area-m']]: size === 'm',
+          [styles['info-area-s']]: size === 's',
+        })}>
         <span className={styles.nickname}>{nickname}</span>
         <span className={styles.date}>{date}</span>
       </div>
