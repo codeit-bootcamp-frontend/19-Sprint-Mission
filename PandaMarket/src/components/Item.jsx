0caa821437c3a6a1d63725f94da9436b25d46c0e
k0nghaa <k0nghaalee@gmail.com>
2025-11-10 19:42:12 +0900

    refactor: Item 컴포넌트 스타일을 CSS Module로 변경

--- PandaMarket/src/components/Item.jsx
+++ PandaMarket/src/components/Item.jsx
@@ -1,18 +1,18 @@
-import "./Item.css";
+import style from "./Item.module.css";
 import likeIcon from "../assets/like-icon.svg";
 
 const Item = ({ product }) => {
   const { name, price, images } = product;
 
   return (
-    <div className="item">
-      <img className="card" src={images[0]} />
-      <div className="card-text">
-        <div className="title">{name}</div>
-        <div className="price">{price.toLocaleString()}원</div>
-        <div className="like">
-          <img className="icon" src={likeIcon}></img>
-          <div className="count">240</div>
+    <div className={style.item}>
+      <img className={style.cardImg} src={images[0]} />
+      <div className={style.cardText}>
+        <div className={style.title}>{name}</div>
+        <div className={style.price}>{price.toLocaleString()}원</div>
+        <div className={style.like}>
+          <img className={style.icon} src={likeIcon}></img>
+          <div className={style.count}>240</div>
         </div>
       </div>
     </div>
