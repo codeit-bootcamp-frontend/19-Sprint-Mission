01a09d3dec0f717df6d26c9c435eb9a83f83c7c3
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-10 15:38:01 +0900

    ✨ Feat: 상품 상세페이지 라우팅 추가

---
+++ src/pages/DetailItem.jsx
@@ -0,0 +1,5 @@
+const DetailItem = () => {
+  return <div>디테일이요~</div>;
+};
+
+export default DetailItem;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import MainLayout from '@/layouts/mainLayout/MainLayout';
 import AddItem from '@/pages/AddItem';
 import Boards from '@/pages/Boards';
+import DetailItem from '@/pages/DetailItem';
 import Home from '@/pages/Home';
 import Items from '@/pages/Items';
 import Login from '@/pages/Login';
@@ -13,9 +14,12 @@ const Router = () => {
       <Routes>
         <Route index element={<Home />} />
         <Route element={<MainLayout />}>
-          <Route path="/items" element={<Items />} />
           <Route path="/boards" element={<Boards />} />
           <Route path="/additem" element={<AddItem />} />
+          <Route path="/items">
+            <Route index element={<Items />} />
+            <Route path=":id" element={<DetailItem />} />
+          </Route>
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
