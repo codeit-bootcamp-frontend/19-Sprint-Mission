af88df5a315b3dffb38810bdaa5cc52915af6308
k0nghaa <k0nghaalee@gmail.com>
2025-11-10 17:31:44 +0900

    feat: api 관련 유틸 파일 추가 (utils/api.js)

---
+++ PandaMarket/src/utils/api.js
@@ -0,0 +1,56 @@
+const BASE_URL = "https://panda-market-api.vercel.app/";
+
+export async function fetchBestProducts({ pageSize = 4 } = {}) {
+  const url = `${BASE_URL}products?pageSize=${pageSize}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.list;
+  } catch (error) {
+    console.error("데이터 가져오기 오류:", error);
+    throw error;
+  }
+}
+
+export async function fetchAllProducts({
+  page = 1,
+  pageSize = 10,
+  orderBy = "recent",
+} = {}) {
+  const url = `${BASE_URL}products?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.list;
+  } catch (error) {
+    console.error("데이터 가져오기 오류:", error);
+    throw error;
+  }
+}
+
+// fetch(test)
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("ERROR:" + response.statusText);
+//     }
+
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log("가져온 데이터 확인:", data);
+//   })
+//   .catch((error) => {
+//     console.error("데이터 가져오기 오류:", error);
+//   });
