42416456e12e4da328f8955222594ddd35fcb708
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-11 00:38:52 +0900

    ✨ Feat: ProfileImg 컴포넌트 size props 추가

--- src/components/common/profileImg/ProfileImg.jsx
+++ src/components/common/profileImg/ProfileImg.jsx
@@ -1,7 +1,8 @@
 import images from '@/assets/images/images';
 
-const ProfileImg = () => {
-  return <img width={40} src={images.profile} alt="프로필 이미지" />;
+const ProfileImg = ({ size = 'm' }) => {
+  const width = size === 'm' ? 40 : 32;
+  return <img width={width} src={images.profile} alt="프로필 이미지" />;
 };
 
 export default ProfileImg;
