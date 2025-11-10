075574fbaaf9ad75a8c05bfafde6ecf2779c7c3d
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-11 00:08:25 +0900

    🎨 Style: 좋아오 버튼 반응형 디자인 추가

--- src/components/itemDetail/likeButton/LikeButton.jsx
+++ src/components/itemDetail/likeButton/LikeButton.jsx
@@ -4,11 +4,7 @@ import styles from './LikeButton.module.css';
 const LikeButton = ({ favoriteCount, isFavorite }) => {
   return (
     <button type="button" aria-label="상품 좋아요" className={styles.container}>
-      {isFavorite ? (
-        <icons.HeartActiveIcon />
-      ) : (
-        <icons.HeartIcon width={32} height={32} />
-      )}
+      {isFavorite ? <icons.HeartActiveIcon /> : <icons.HeartIcon />}
       <span className={styles.text}>{favoriteCount}</span>
     </button>
   );
