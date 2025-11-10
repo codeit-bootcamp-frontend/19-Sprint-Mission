633aca293dcd3b8b962d732bdba4011121a5d24c
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-11 01:36:39 +0900

    ✨ Feat: useFetchComments 훅 구현 및 적용

--- src/pages/ItemDetail.jsx
+++ src/pages/ItemDetail.jsx
@@ -1,14 +1,24 @@
+import classNames from 'classnames';
 import { Link, useParams } from 'react-router';
 import Icons from '@/assets/icons/icons';
 import Button from '@/components/common/button/Button';
 import CommentForm from '@/components/itemDetail/commentForm/CommentForm';
+import CommentList from '@/components/itemDetail/commentList/CommentList';
 import ProductDetail from '@/components/itemDetail/productDetail/ProductDetail';
+import useFetchComments from '@/hooks/useFetchComments';
 import useFetchProduct from '@/hooks/useFetchProduct';
 import styels from '@/style/page/ItemDetail.module.css';
 
 const ItemDetail = () => {
   const { id } = useParams();
   const { product, loading, error } = useFetchProduct(id);
+  const {
+    comments,
+    fetchMore,
+    loading: commentLoading,
+    isEnd,
+    error: commentError,
+  } = useFetchComments({ productId: id });
 
   const handleProductEdit = () => {
     // TODO: 수정 로직 이후 미션에서 구현
@@ -16,6 +26,16 @@ const ItemDetail = () => {
   };
 
   const handleProductDelete = () => {
+    // TODO: 삭제 로직 이후 미션에서 구현
+    console.log('삭제하기');
+  };
+
+  const handleCommentEdit = () => {
+    // TODO: 수정 로직 이후 미션에서 구현
+    console.log('수정하기');
+  };
+
+  const handleCommentDelete = () => {
     // TODO: 삭제 로직 이후 미션에서 구현
     console.log('삭제하기');
   };
@@ -31,13 +51,40 @@ const ItemDetail = () => {
   }
 
   return (
-    <div className={styels.container}>
+    <div className={classNames(styels['common-layout'], styels.container)}>
       <ProductDetail
         product={product}
         onEdit={handleProductEdit}
         onDelete={handleProductDelete}
       />
-      <CommentForm />
+      <div
+        className={classNames(styels['common-layout'], styels['comment-area'])}>
+        <CommentForm />
+
+        {commentLoading && <div>댓글을 불러오는 중입니다...</div>}
+
+        {commentError && <div>에러가 발생했습니다: {commentError.message}</div>}
+
+        {!commentLoading && !commentError && (
+          <div className={styels['comment-list']}>
+            <CommentList
+              comments={comments}
+              onEdit={handleCommentEdit}
+              onDelete={handleCommentDelete}
+            />
+
+            {!isEnd && (
+              <Button
+                onClick={fetchMore}
+                disabled={commentLoading}
+                size="xs"
+                className={styels['load-more-btn']}>
+                댓글 더보기
+              </Button>
+            )}
+          </div>
+        )}
+      </div>
       <Button as={Link} to={'/items'} size="m">
         목록으로 돌아가기
         <Icons.BackIcon />
