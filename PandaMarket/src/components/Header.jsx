555901198b5dfe72cd7c5efb3f57cffc72b4c163
k0nghaa <k0nghaalee@gmail.com>
2025-11-11 23:04:26 +0900

    fix: Header 배경색 및 padding 수정, 반응형 내부 요소 정렬 개선

--- PandaMarket/src/components/Header.jsx
+++ PandaMarket/src/components/Header.jsx
@@ -5,12 +5,14 @@ import profile from "../assets/profile.svg";
 const Header = () => {
   return (
     <div className={style.header}>
-      <img className={style.logo} src={headerLogo} />
-      <div className={style.tab}>
-        <button className={style.board}>자유게시판</button>
-        <button className={style.market}>중고마켓</button>
+      <div className={style.headerInner}>
+        <img className={style.logo} src={headerLogo} />
+        <div className={style.tab}>
+          <button className={style.board}>자유게시판</button>
+          <button className={style.market}>중고마켓</button>
+        </div>
+        <img className={style.profile} src={profile} />
       </div>
-      <img className={style.profile} src={profile} />
     </div>
   );
 };
