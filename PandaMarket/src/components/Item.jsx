f4a831c78341b009568c9783d2c513eb01db9690
k0nghaa <k0nghaalee@gmail.com>
2025-11-11 20:16:25 +0900

    refactor: Item 이미지 사이즈 props 적용으로 범용성 향상

--- PandaMarket/src/components/Item.jsx
+++ PandaMarket/src/components/Item.jsx
@@ -1,12 +1,12 @@
 import style from "./Item.module.css";
 import likeIcon from "../assets/like-icon.svg";
 
-const Item = ({ product }) => {
+const Item = ({ product, imgClass }) => {
   const { name, price, images } = product;
 
   return (
     <div className={style.item}>
-      <img className={style.cardImg} src={images[0]} />
+      <img className={`${style.cardImg} ${imgClass}`} src={images[0]} />
       <div className={style.cardText}>
         <div className={style.title}>{name}</div>
         <div className={style.price}>{price.toLocaleString()}원</div>
