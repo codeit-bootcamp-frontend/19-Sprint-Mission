40693a1d30e8b985caef9120846eb0973cb43c07
k0nghaa <k0nghaalee@gmail.com>
2025-11-11 23:24:43 +0900

    feat: Header 로고 및 중고마켓 탭 링크 기능 구현

--- PandaMarket/src/components/Header.jsx
+++ PandaMarket/src/components/Header.jsx
@@ -1,19 +1,24 @@
 import style from "./Header.module.css";
 import headerLogo from "../assets/logo.svg";
 import profile from "../assets/profile.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className={style.header}>
+    <header className={style.header}>
       <div className={style.headerInner}>
-        <img className={style.logo} src={headerLogo} />
+        <Link to="/">
+          <img className={style.logo} src={headerLogo} />
+        </Link>
         <div className={style.tab}>
           <button className={style.board}>자유게시판</button>
-          <button className={style.market}>중고마켓</button>
+          <Link to="/items">
+            <button className={style.market}>중고마켓</button>
+          </Link>
         </div>
         <img className={style.profile} src={profile} />
       </div>
-    </div>
+    </header>
   );
 };
 
