a955c81dba05c364b29a186ac021b0673931df54
k0nghaa <k0nghaalee@gmail.com>
2025-11-11 22:47:27 +0900

    refactor: Header CSS를 CSS Modules로 마이그레이션

--- PandaMarket/src/components/Header.jsx
+++ PandaMarket/src/components/Header.jsx
@@ -1,16 +1,16 @@
-import "./Header.css";
+import style from "./Header.module.css";
 import headerLogo from "../assets/logo.svg";
 import profile from "../assets/profile.svg";
 
 const Header = () => {
   return (
-    <div className="header">
-      <img className="logo" src={headerLogo} />
-      <div className="tap">
-        <button className="board">자유게시판</button>
-        <button className="market">중고마켓</button>
+    <div className={style.header}>
+      <img className={style.logo} src={headerLogo} />
+      <div className={style.tab}>
+        <button className={style.board}>자유게시판</button>
+        <button className={style.market}>중고마켓</button>
       </div>
-      <img className="profile" src={profile} />
+      <img className={style.profile} src={profile} />
     </div>
   );
 };
