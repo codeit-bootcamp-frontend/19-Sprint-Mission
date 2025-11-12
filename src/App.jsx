682dd858a20ad8b3838c7cf7ff4e76dd6b5e3999
yujin-fe <hyujin1224@gmail.com>
2025-11-12 15:46:23 +0900

    feat: /items, /additem 페이지 라우트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path={"/items"} element={<Items />} />
+        <Route path={"/additem"} element={<AddItem />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
