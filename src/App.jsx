b103c607bfe8c7428c9cf45f0e81b1e3a53f8c8a
yujin-fe <hyujin1224@gmail.com>
2025-11-13 18:06:53 +0900

    feat: 헤더 ui 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import "./assets/css/global.scss";
 import { Route, Routes } from "react-router-dom";
 import Items from "./pages/Items";
 import AddItem from "./pages/AddItem";
+import Layout from "@/layout/Layout";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path={"/items"} element={<Items />} />
-        <Route path={"/additem"} element={<AddItem />} />
+        <Route path="/" element={<Layout />}>
+          <Route path={"/items"} element={<Items />} />
+          <Route path={"/additem"} element={<AddItem />} />
+        </Route>
       </Routes>
     </div>
   );
