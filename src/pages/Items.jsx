bf5d4706ad8d5ba98215794bed6026b8fa79532d
yujin-fe <hyujin1224@gmail.com>
2025-11-13 11:09:48 +0900

    feat:items 페이지 임시제작

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,5 +1,31 @@
+import Card from "../components/Card";
+import { useState, useEffect } from "react";
+import { getProducts } from "../api/product";
+import "./Items.scss";
 const Items = () => {
-  return <div>/Items</div>;
+  const [items, setItems] = useState([]);
+
+  const fetchProductData = async () => {
+    const params = {
+      page: 1,
+      pageSize: 10,
+      orderBy: "recent",
+    };
+    const productData = await getProducts(params);
+    setItems(productData.list);
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, []);
+
+  return (
+    <div className="cards-container">
+      {items?.map((item) => (
+        <Card {...item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Items;
