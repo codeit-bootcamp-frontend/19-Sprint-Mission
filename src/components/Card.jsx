f912088b8edf09748c7300320041e3776788ef4b
yujin-fe <hyujin1224@gmail.com>
2025-11-13 11:08:09 +0900

    feat: Card 컴포넌트 제작

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,21 @@
-const Card = () => {
-  return <div>Card</div>;
+import "./Card.scss";
+import heartIcon from "../assets/ic_heart.svg";
+
+const Card = (props) => {
+  const { images, name, price, favoriteCount } = props;
+  return (
+    <div className="Card">
+      <img className="card-image" src={images} />
+      <div className="card-info">
+        <h1 className="card-name">{name}</h1>
+        <p className="card-price">{price}</p>
+        <p className="card-favorite">
+          <img src={heartIcon} />
+          {favoriteCount}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
