0c702d92355ae74966a0b03c72b59fa836483935
yujin-fe <hyujin1224@gmail.com>
2025-12-11 19:15:31 +0900

    ✨ Feat: formData 상태 추가

--- src/pages/AddItem.jsx
+++ src/pages/AddItem.jsx
@@ -8,32 +8,59 @@ import { useState } from "react";
 
 const AddItem = () => {
   const [tags, setTags] = useState([]);
+  const [formData, setFormData] = useState({
+    image: "",
+    name: "",
+    introduction: "",
+    price: "",
+  });
+
+  const handleInput = (value, input) => {
+    setFormData((prev) => ({ ...prev, [input]: value }));
+  };
+
+  const validForm =
+    formData.image &&
+    formData.name &&
+    formData.introduction &&
+    formData.price &&
+    tags.length > 0;
+
   return (
     <main className="AddItem">
       <form className="form-container">
         <div className="item-submit-container">
           <span className="title-text item-submit-text">상품 등록하기</span>
-          <Button className="item-submit-btn" disabled={true}>
+          <Button className="item-submit-btn" disabled={!validForm}>
             등록
           </Button>
         </div>
         <div className="form-input-container">
-          <ImageForm title="상품 이미지" />
+          <ImageForm
+            title="상품 이미지"
+            onFileChange={(value) => handleInput(value, "image")}
+          />
           <Input
+            value={formData.name}
             name="item-name"
             title="상품명"
             placeholder="상품명을 입력해주세요"
+            onChange={(value) => handleInput(value, "name")}
           />
           <Textarea
+            value={formData.introduction}
             name="item-introduction"
             title="상품 소개"
             placeholder="상품소개를 입력해주세요"
+            onChange={(value) => handleInput(value, "introduction")}
           />
           <Input
+            value={formData.price}
             name="item-price"
             type="number"
             title="판매가격"
-            placeholder="상품명을 입력해주세요"
+            placeholder="가격을 입력해주세요"
+            onChange={(value) => handleInput(value, "price")}
           />
           <TagInput
             name="item-tag"
