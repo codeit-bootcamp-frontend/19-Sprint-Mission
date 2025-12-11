b82048e85609af88932d18fe4e9f205063568e11
yujin-fe <hyujin1224@gmail.com>
2025-12-11 19:25:39 +0900

    🎨 Style: 중고마켓 색상 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,7 +2,11 @@ import "./Header.scss";
 import pandaLogo from "@/assets/images/panda_logo.svg";
 import profile from "@/assets/images/profile.svg";
 import pandaText from "@/assets/images/panda_text.svg";
+import { useLocation } from "react-router-dom";
 const Header = () => {
+  const { pathname } = useLocation();
+  const additemPage = pathname.startsWith("/additem");
+  const itemsPage = pathname.startsWith("/items");
   return (
     <div className="Header">
       <img className="logo-image" src={pandaLogo} />
@@ -12,7 +16,9 @@ const Header = () => {
           <li>
             <a>자유게시판</a>
           </li>
-          <li>
+          <li
+            className={`${additemPage || itemsPage ? "secondhand-market" : ""}`}
+          >
             <a>중고마켓</a>
           </li>
         </ul>
